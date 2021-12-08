ddf43434c7eb4ed1c1f44a8d6fba3bf4a6b5ca38
nickherrig <neherrig@gmail.com>
2021-12-07 19:45:38 -0600

    Complete day three part one

--- 2021/three/one.go
+++ 2021/three/one.go
@@ -4,7 +4,56 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
+
+func bitSummary(input []string, position int) map[string]int64 {
+	s := map[string]int64{}
+	for _, val := range input {
+		bit := string(val[position])
+		s[bit]++
+	}
+
+	return s
+}
+
+func gammaRate(input []string) (int64, error) {
+	// Most common bit
+
+	r := ""
+	bitLength := len(input[0])
+
+	for i := 0; i < bitLength; i++ {
+		s := bitSummary(input, i)
+
+		if s["1"] > s["0"] {
+			r += "1"
+		} else {
+			r += "0"
+		}
+	}
+
+	return strconv.ParseInt(r, 2, 64)
+}
+
+func epsilonRate(input []string) (int64, error) {
+	// Most common bit
+
+	r := ""
+	bitLength := len(input[0])
+
+	for i := 0; i < bitLength; i++ {
+		s := bitSummary(input, i)
+
+		if s["1"] > s["0"] {
+			r += "0"
+		} else {
+			r += "1"
+		}
+	}
+
+	return strconv.ParseInt(r, 2, 64)
+}
 
 func main() {
 	file, err := os.Open("input.txt")
@@ -21,6 +70,16 @@ func main() {
 		input = append(input, t)
 	}
 
-	fmt.Println(input)
+	g, err := gammaRate(input)
+	if err != nil {
+		fmt.Print("error converting gamma rate")
+	}
+
+	e, err := epsilonRate(input)
+	if err != nil {
+		fmt.Print("error converting gamma rate")
+	}
+
+	fmt.Println(g * e)
 
 }
