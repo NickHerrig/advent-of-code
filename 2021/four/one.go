c3d6d4e35f1e48cc135deb97514dee912e32f384
nickherrig <neherrig@gmail.com>
2021-12-09 21:28:53 -0600

    Scafolding, board setup, methods on board

--- 2021/four/one.go
+++ 2021/four/one.go
@@ -1,24 +1,93 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func (b *board) update(ball string) error {
+	return nil
+}
+
+func (b *board) bingo() bool {
+	return false
+}
+
+func (b *board) sum() int {
+	return 1000
+}
+
+type board struct {
+	Numbers [][]string
+}
+
 func main() {
+	// Open the input file
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("issue opening input file")
+		os.Exit(1)
+	}
+	defer file.Close()
 
-	numbers := []string{"1", "2", "3", "4"}
+	// create a scanner
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+
+	// read first line, split on a comma, and load bingo cage, close chanel for ranging
+	line := scanner.Text()
+	numbers := strings.Split(line, ",")
 	c := newCage(numbers)
+	close(c)
 
-	// This code will close the channel, alow for looping...
-	//	close(c)
-	//	for ball := range c {
-	//		fmt.Println(ball)
-	//	}
+	// read lines into slice, ignoring blank lines
+	lines := []string{}
+	for scanner.Scan() {
+		if len(scanner.Text()) > 1 {
+			lines = append(lines, scanner.Text())
+		}
+	}
 
-	// If left open we can just read entries..
+	// Initalize a slice of boards
+	boards := []*board{}
+	length := len(lines)
+	chunk := 5
+	for i := chunk; i < length; i += chunk {
+		b := newBoard(lines[i-chunk : i])
+		boards = append(boards, b)
+	}
 
-	//fmt.Println(<-c)
-	//fmt.Println(<-c)
-	//fmt.Println(<-c)
-	//fmt.Println(<-c)
+	// Main loop through balls, and update board
+	for ball := range c {
+		for _, board := range boards {
+			board.update(ball)
+			if sum := board.sum(); board.bingo() {
+				fmt.Println("Winner!")
+				b, err := strconv.Atoi(ball)
+				if err != nil {
+					fmt.Println("issue convering ball to int")
+				}
+				answer := sum * b
+				fmt.Println(answer)
+			}
+		}
 
-	// Is a slice better? here, Kind like the stack queue
+	}
+
+}
+
+func newBoard(lines []string) *board {
+
+	s := [][]string{}
+	for _, line := range lines {
+		vals := strings.Split(line, " ")
+		s = append(s, vals)
+	}
+
+	return &board{Numbers: s}
 
 }
 
