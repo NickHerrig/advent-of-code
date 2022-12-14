3af94f81313fd18a7fdf28ea6c8c044110f92611
nickherrig <neherrig@gmail.com>
2022-12-14 07:54:32 -0600

    Day 6 part one

---
+++ 2022/6/trouble.py
@@ -0,0 +1,26 @@
+def parse_data(file):
+    return file.pop(0)
+
+
+def problem_one(stream):
+
+    for start, end in enumerate(range(4, len(stream)+1)):
+        compare_chars = stream[start:end]
+
+        if not any(compare_chars.count(c) > 1 for c in compare_chars):
+            print(end)
+            break
+
+def main():
+
+    file = []
+    with open("input.txt") as f:
+        file = f.readlines()
+
+    stream = parse_data(file)
+    problem_one(stream)
+
+
+
+if __name__=="__main__":
+    main()
